1a6086e6b26eb5ed31dd6281f5d027657acc7d92
SWW-LAB <57850488+SWW-LAB@users.noreply.github.com>
2020-05-31 21:30:26 +0800

    Update get_patch.py

--- 02-Source_Code_of_Simulation_data/code/get_patch.py
+++ 02-Source_Code_of_Simulation_data/code/get_patch.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 '''
-从图片中获取倾斜的区域图,先将图片反向旋转相应的角度,再截取相应的区域
+Obtain the slant area from the picture:
+first rotate the picture reversely by the corresponding angle, and then intercept the corresponding area
 '''
 
 from PIL import Image
@@ -10,19 +11,19 @@ import random
 def get_patch(depth_path, num, *args):
     '''
 
-    :param depth_path: 文件位置
-    :param num: 旋转角度,大于等于0,小于90,不适用于90
-    :param args: patch的中心位置,以及patch的大小
+    :param depth_path: File location
+    :param num: Rotation angle, greater than or equal to 0, less than 90, not applicable to 90
+    :param args:The center position and size of the patch
     :return: patch
     '''
     img = Image.open(depth_path)
     img_size = img.size
     radians = math.radians(num)
 
-    # 图像旋转后patch的中心点坐标,顺时针为正,顺时针旋转
+    # The coordinates of the center point of the patch after the image is rotated, clockwise is positive
     x = int(round((args[0][0]-img_size[0]/2)*math.cos(radians)-(args[0][1]-img_size[1]/2)*math.sin(radians)+img_size[0]/2))
     y = int(round((args[0][0]-img_size[0]/2)*math.sin(radians)+(args[0][1]-img_size[1]/2)*math.cos(radians)+img_size[1]/2))
-    img2 = img.rotate(-num)  #Image逆时针为正
+    img2 = img.rotate(-num)  # clockwise is positive
     patch = img2.crop((x-args[1][0]/2, y-args[1][1]/2, x+args[1][0]/2, y+args[1][1]/2))
     return patch
 
@@ -36,7 +37,7 @@ if __name__ == '__main__':
     #     center = [295, 370]
     #     patch_size = [128, 128]
     #     angle = 0
-    #     img = get_patch(depth_path, angle, center, patch_size)  #输入图片路径\patch的中心位置\旋转角度\patch的大小
+    #     img = get_patch(depth_path, angle, center, patch_size)  #Enter the picture path \central position of patch \rotation angle \patch size
     #     save_path = '/home/well/simulation_data2.0/test2/overall/' + str(i) + '.jpg'
     #     img.save(save_path)
 
@@ -52,19 +53,19 @@ if __name__ == '__main__':
         patch_size = [200, 200]
         angle = 0
 
-        img = get_patch(rgb_path, angle, center, patch_size)  #输入图片路径\patch的中心位置\旋转角度\patch的大小
+        img = get_patch(rgb_path, angle, center, patch_size)  #Enter the picture path \central position of patch \rotation angle \patch size
         save_path = '/home/well/simulation_data3.0/test5/train_dataset1.0/rgb_patches/' + str(i) + '.jpg'
         img.save(save_path)
 
-        img = get_patch(depth_path, angle, center, patch_size)  #输入图片路径\patch的中心位置\旋转角度\patch的大小
+        img = get_patch(depth_path, angle, center, patch_size)  
         save_path = '/home/well/simulation_data3.0/test5/train_dataset1.0/depth_patches/' + str(i) + '.jpg'
         img.save(save_path)
 
-        img = get_patch(cur_path, angle, center, patch_size)  #输入图片路径\patch的中心位置\旋转角度\patch的大小
+        img = get_patch(cur_path, angle, center, patch_size)  
         save_path = '/home/well/simulation_data3.0/test5/train_dataset1.0/cur_patches/' + str(i) + '.jpg'
         img.save(save_path)
 
-        img = get_patch(norm_path, angle, center, patch_size)  #输入图片路径\patch的中心位置\旋转角度\patch的大小
+        img = get_patch(norm_path, angle, center, patch_size)  
         save_path = '/home/well/simulation_data3.0/test5/train_dataset1.0/normal_patches/' + str(i) + '.jpg'
         img.save(save_path)
 
@@ -80,18 +81,18 @@ if __name__ == '__main__':
         patch_size = [200, 200]
         angle = 0
 
-        img = get_patch(rgb_path, angle, center, patch_size)  #输入图片路径\patch的中心位置\旋转角度\patch的大小
+        img = get_patch(rgb_path, angle, center, patch_size)  #Enter the picture path \central position of patch \rotation angle \patch size
         save_path = '/home/well/simulation_data3.0/test5/test_dataset1.0/rgb_patches/' + str(i) + '.jpg'
         img.save(save_path)
 
-        img = get_patch(depth_path, angle, center, patch_size)  #输入图片路径\patch的中心位置\旋转角度\patch的大小
+        img = get_patch(depth_path, angle, center, patch_size)  
         save_path = '/home/well/simulation_data3.0/test5/test_dataset1.0/depth_patches/' + str(i) + '.jpg'
         img.save(save_path)
 
-        img = get_patch(cur_path, angle, center, patch_size)  #输入图片路径\patch的中心位置\旋转角度\patch的大小
+        img = get_patch(cur_path, angle, center, patch_size) 
         save_path = '/home/well/simulation_data3.0/test5/test_dataset1.0/cur_patches/' + str(i) + '.jpg'
         img.save(save_path)
 
-        img = get_patch(norm_path, angle, center, patch_size)  #输入图片路径\patch的中心位置\旋转角度\patch的大小
+        img = get_patch(norm_path, angle, center, patch_size)  
         save_path = '/home/well/simulation_data3.0/test5/test_dataset1.0/normal_patches/' + str(i) + '.jpg'
         img.save(save_path)
